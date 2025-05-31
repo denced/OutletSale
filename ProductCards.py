0835ad5ddb5553960139394c2d320ef4fc893d5b
Denis Cedeno <30967379+denced@users.noreply.github.com>
2025-05-31 19:46:57 -0400

    Correct image URLs to use media.githubusercontent.com path

--- ProductCards.py
+++ ProductCards.py
@@ -82,11 +82,11 @@ def generate_product_cards(grouped_data, image_folder):
         if not os.path.exists(os.path.join(image_folder, image_name)):
             image_name = None  # No image available
 
-        # Update image URL to use GitHub Pages location
+        # Update image URL to use the corrected GitHub Pages location
         if image_name:
-            image_url = f"https://github.com/denced/OutletSale/blob/main/images/{image_name}"
+            image_url = f"https://media.githubusercontent.com/media/denced/OutletSale/main/images/{image_name}"
         else:
-            image_url = "https://github.com/denced/OutletSale/blob/main/images/placeholder.jpg"
+            image_url = "https://media.githubusercontent.com/media/denced/OutletSale/main/images/placeholder.jpg"
 
         # Get the colorName
         color_name = get_color_name(color_key)
